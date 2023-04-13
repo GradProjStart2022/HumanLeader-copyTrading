cf9fd64f1eaae065fd851f4cd9ac2bdcf8a48095
gwon6482 <pdsjw991020@gmail.com>
2023-04-13 16:10:14 +0900

    edit app.js

--- ct_main/app.js
+++ ct_main/app.js
@@ -4,25 +4,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 // swaggrt Router
-var v1swaggerRouter = require('./moudes/EV_module/router/v1/index')
-
-// index Router
+var v1swaggerRouter = require('./moudes/EV_module/router/v1/index');
 var indexRouter = require('./moudes/EV_module/router/index');
-
-// Test Router
-var TEST_Router = require('./moudes/EV_module/Router/TEST_Router')
-
-/**  신규 이벤트   */
-
-// 회원가입
+var TEST_Router = require('./moudes/EV_module/Router/TEST_Router');
 var UR_Router = require('./moudes/EV_module/Router/UR_Router');
-
-// APP 라우터
-var LR_Router = require('./moudes/EV_module/Router/LR_Router')
-
-
-// Monitor server Router
-var trade_Router = require('./moudes/EV_module/router/TR_Router')
+var LR_Router = require('./moudes/EV_module/Router/LR_Router');
+var trade_Router = require('./moudes/EV_module/router/TR_Router');
 
 
 var app = express();
@@ -40,17 +27,17 @@ app.use('/', indexRouter);
 // swagger 
 app.use("/v1",v1swaggerRouter);
 
-
-// Monitor server apis
+// trade
 app.use('/trade',trade_Router);
 
 // test 
 app.use('/test',TEST_Router);
 
-// USER event
+// USER 
 app.use('/user',UR_Router);
 
-// APP event 
+// leader 
 app.use('/leader',LR_Router);
+
 
 module.exports = app;
